d67dcba03b0d5404a16ea934b6fe6a4b01c0ee36
Bob Uva <robertuva@gmail.com>
2014-08-10 13:52:55 -0700

    Initial source and project files.

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+## Filename: run_analysis.R
+##
+##  Original: 8/10/2014 Bob Uva
+##
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+##
+##  Data set locations relative to project: ../UCI HAR Dataset/test and ../UCI HAR Dataset/train
+##
+
+run_analysis <- function(x) {}
