59927a9e6678dcf3ce2195d7c74d26844ac321b5
Bob Uva <robertuva@gmail.com>
2014-08-16 17:42:50 -0700

    Read in training/test data. Not modularized yet.

--- run_analysis.R
+++ run_analysis.R
@@ -11,4 +11,32 @@
 ##  Data set locations relative to project: ../UCI HAR Dataset/test and ../UCI HAR Dataset/train
 ##
 
-run_analysis <- function(x) {}
+run_analysis <- function(x) {
+  
+  # Set working directory
+  setwd("C:/DSCoursera/GettingAndCleaningData/Project/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+  
+  # Read training data
+  print("Reading training data...")
+  subjectPerSample <- read.table("train/subject_train.txt")
+  activityPerSample <- read.table("train/y_train.txt")
+  samples <- read.table("train/x_train.txt")
+  stopifnot(nrow(subjectPerSample) == nrow(activityPerSample == nrow(samples)))
+  
+  # Read feature labels
+  print("Reading Feature Labels...")
+  featureLabels <- read.table("features.txt")
+  stopifnot(ncol(samples) == nrow(featureLabels))
+  print("Now to assign featureLabels as training samples column names")
+  colnames(samples) <- featureLabels[,2]
+  
+  # Read test data
+  print("Reading test data...")
+  subjectPerSampleTest <- read.table("test/subject_test.txt")
+  activityPerSampleTest <- read.table("test/y_test.txt")
+  samplesTest <- read.table("test/x_test.txt")
+  stopifnot(nrow(subjectPerSampleTest) == nrow(activityPerSampleTest == nrow(samplesTest)))
+  print("Now to assign featureLabels as test samples column names")
+  colnames(samplesTest) <- featureLabels[,2]
+  head(samplesTest)
+}
