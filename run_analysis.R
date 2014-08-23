8c3d521cfa99ef9bd9101792d857db2c0ca8a246
Bob Uva <robertuva@gmail.com>
2014-08-23 16:12:59 -0700

    Retrieve mean/sd columns from merged set.

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,9 @@
 ##
 ##  Original: 8/10/2014 Bob Uva
 ##
+##  Prerequisites:
+##    Uses the fBasics package. Make sure that package is installed and available.
+##
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 ## 3. Uses descriptive activity names to name the activities in the data set
@@ -15,30 +18,60 @@ run_analysis <- function(x) {
   
   # Set working directory
   setwd("C:/DSCoursera/GettingAndCleaningData/Project/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+
+  # Read feature labels
+  print("1. Reading Feature Labels...")
+  featureLabels <- read.table("features.txt")
   
   # Read training data
-  print("Reading training data...")
-  subjectPerSample <- read.table("train/subject_train.txt")
-  activityPerSample <- read.table("train/y_train.txt")
-  samples <- read.table(colClasses="numeric", "train/x_train.txt")
-  stopifnot(nrow(subjectPerSample) == nrow(activityPerSample == nrow(samples)))
+  print("2. Reading training data...")
+  subjectPerSampleTrain <- read.table("train/subject_train.txt")
+  activityPerSampleTrain <- read.table("train/y_train.txt")
+  samplesTrain <- read.table(colClasses="numeric", "train/x_train.txt")
+  stopifnot(nrow(subjectPerSampleTrain) == nrow(activityPerSampleTrain == nrow(samplesTrain)))
   
-  # Read feature labels
-  print("Reading Feature Labels...")
-  featureLabels <- read.table("features.txt")
-  stopifnot(ncol(samples) == nrow(featureLabels))
-  print("Now to assign featureLabels as training samples column names")
-  colnames(samples) <- featureLabels[,2]
+  stopifnot(ncol(samplesTrain) == nrow(featureLabels))
+  print("     Assigning Feature Labels as column names")
+  colnames(samplesTrain) <- featureLabels[,2]
   
   # Read test data
-  print("Reading test data...")
+  print("3. Reading test data...")
   subjectPerSampleTest <- read.table("test/subject_test.txt")
   activityPerSampleTest <- read.table("test/y_test.txt")
   samplesTest <- read.table(colClasses="numeric", "test/x_test.txt")
   stopifnot(nrow(subjectPerSampleTest) == nrow(activityPerSampleTest == nrow(samplesTest)))
-  print("Now to assign featureLabels as test samples column names")
+  print("     Assigning Feature Labels as column names")
   colnames(samplesTest) <- featureLabels[,2]
   
   # Merge training and test data sets
-  samplesMerged <- rbind(samples, samplesTest)
+  print("4. Merging training and test data sets...")
+  samplesMerged <- rbind(samplesTrain, samplesTest)
+  
+  # Find the column indices where the word "angle" is used. This should be excluded.
+  #angleColumnIndices <- grep(pattern="angle", x=featureLabels[,2])
+  
+  # Find the column indices where the data is a mean.
+  # Note: This does not include 'meanFreq' columns. If that is not correct, 
+  # I can change to grepping for 'mean' and for 'angle' and removing the angle
+  # columns from the indices list.
+  
+  meanColumnIndices <- grep(pattern="mean()", x=featureLabels[,2], fixed=TRUE, value=FALSE)
+  #remaining <- lapply(meanColumnIndices, function(x) { !(x%in%angleColumnIndices)})
+  #remain <- unlist(remaining)
+  #means <- meanColumnIndices[remain]
+  print(meanColumnIndices)
+  print(length(meanColumnIndices))
+  # Find the column indices where the data is a standard deviation
+  sdColumnIndices <- grep(pattern="std", x=featureLabels[,2])
+  
+  # Create the data set that contains
+  #   1. Descriptive Activity Names 
+  #   2. Descriptive Variable Names (for mean and SD columns)
+  #
+  #dfResults <- data.frame()
+  
+  # Create a tidy data set that contains
+  #   1. the average of each variable for each activity and subject.
 }
+
+library
