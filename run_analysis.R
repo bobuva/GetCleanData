336b2d44b9d25030184ad356dc0fed63ec629162
Bob Uva <robertuva@gmail.com>
2014-08-23 23:59:23 -0700

    set row.names to NULL in read.table calls.

--- run_analysis.R
+++ run_analysis.R
@@ -18,46 +18,73 @@ run_analysis <- function(x) {
 
   # Read feature labels
   print("1. Reading Feature Labels...")
-  featureLabels <- read.table("features.txt")
+  featureLabels <- read.table("features.txt", row.names=NULL)
+  
+  # Read activity labels
+  activityLabels <- read.table("activity_labels.txt")
   
   # Read training data
   print("2. Reading training data...")
-  subjectPerSampleTrain <- read.table("train/subject_train.txt")
-  activityPerSampleTrain <- read.table("train/y_train.txt")
-  samplesTrain <- read.table(colClasses="numeric", "train/x_train.txt")
+  subjectPerSampleTrain <- read.table("train/subject_train.txt", row.names=NULL)
+  activityPerSampleTrain <- read.table("train/y_train.txt", row.names=NULL)
+  print(length(activityPerSampleTrain))
+  print(class(activityPerSampleTrain))
+  print(names(activityPerSampleTrain))
+  samplesTrain <- read.table(colClasses="numeric", "train/x_train.txt", row.names=NULL)
   stopifnot(nrow(subjectPerSampleTrain) == nrow(activityPerSampleTrain == nrow(samplesTrain)))
   
+  # Replace activity number per sample with descriptive label for the activity
+  #activityPerSampleTrain$V1 <-  activityLabels[activityPerSampleTrain,2]
+  
+  print("head of activity in train is now:")
+  print(head(activityPerSampleTrain))
+  #activityPerSampleTrain <- lapply(activityPerSampleTrain,
+  #                                 function(n) {
+  #                                   as.character(activityLabels[n, 2])   
+  #                                })
+  
+  # Verification before assigning feature labels to columns
   stopifnot(ncol(samplesTrain) == nrow(featureLabels))
   print("     Assigning Feature Labels as column names")
-  colnames(samplesTrain) <- featureLabels[,2]
+  colnames(samplesTrain) <- featureLabels[, 2]
+  
+  # Bring subject and activity data into training data
+  samplesTrain <- cbind(activityPerSampleTrain, samplesTrain)
+  samplesTrain <- cbind(subjectPerSampleTrain, samplesTrain)
+  colnames(samplesTrain)[1] <- "Subject"
+  colnames(samplesTrain)[2] <- "Activity"
   
   # Read test data
   print("3. Reading test data...")
-  subjectPerSampleTest <- read.table("test/subject_test.txt")
-  activityPerSampleTest <- read.table("test/y_test.txt")
-  samplesTest <- read.table(colClasses="numeric", "test/x_test.txt")
+  subjectPerSampleTest <- read.table("test/subject_test.txt", row.names=NULL)
+  activityPerSampleTest <- read.table("test/y_test.txt", row.names=NULL)
+  samplesTest <- read.table(colClasses="numeric", "test/x_test.txt", row.names=NULL)
+
+  # Replace activity number per sample with descriptive label for the activity
+  
+  #  activityPerSampleTest <- sapply(activityPerSampleTest,
+ #                                  function(n) {
+#                                     as.character(activityLabels[n, 2])   
+#                                   })
+  
+  # Verification before assigning feature labels to columns
   stopifnot(nrow(subjectPerSampleTest) == nrow(activityPerSampleTest == nrow(samplesTest)))
   print("     Assigning Feature Labels as column names")
-  colnames(samplesTest) <- featureLabels[,2]
+  colnames(samplesTest) <- featureLabels[, 2]
   
-  # Merge training and test data sets
+  # Bring subject and activity data into test data
+  samplesTest <- cbind(activityPerSampleTest, samplesTest)
+  samplesTest <- cbind(subjectPerSampleTest, samplesTest)
+  colnames(samplesTest)[1] <- "Subject"
+  colnames(samplesTest)[2] <- "Activity"
+
+# Merge training and test data sets
   print("4. Merging training and test data sets...")
-  samplesMerged <- rbind(samplesTrain, samplesTest)
+#  samplesMerged <- rbind(samplesTrain, samplesTest)
   
-  # Find the column indices where the word "angle" is used. This should be excluded.
-  #angleColumnIndices <- grep(pattern="angle", x=featureLabels[,2])
-  
-  # Find the column indices where the data is a mean.
-  # Note: This does not include 'meanFreq' columns. If that is not correct, 
-  # I can change to grepping for 'mean' and for 'angle' and removing the angle
-  # columns from the indices list.
-  
+  # Find the column indices where the data is a mean.  
   meanColumnIndices <- grep(pattern="mean()", x=featureLabels[,2], fixed=TRUE, value=FALSE)
-  #remaining <- lapply(meanColumnIndices, function(x) { !(x%in%angleColumnIndices)})
-  #remain <- unlist(remaining)
-  #means <- meanColumnIndices[remain]
-  print(meanColumnIndices)
-  print(length(meanColumnIndices))
+
   # Find the column indices where the data is a standard deviation
   sdColumnIndices <- grep(pattern="std", x=featureLabels[,2])
   
